1175b2e38fc5a3de003ab51a9c9c58cc167592d2
Marc Renken <marc.renken@bitbot.eu>
2022-12-29 22:56:39 +0100

    functionLog

--- lib/mixin/logging.dart
+++ lib/mixin/logging.dart
@@ -4,4 +4,10 @@ part of '../logger.dart';
 mixin Logging {
   /// Log a message
   late final log = MessageTemplate(klasse: this);
+
+  /// Create a child template with the given [function]
+  MessageTemplate functionLog(String function) => log.function(function);
+
+  /// Create a child template with the given [klasse]
+  MessageTemplate classLog(Object klasse) => log.klasse(klasse);
 }
