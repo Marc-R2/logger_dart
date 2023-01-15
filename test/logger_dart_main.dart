db9424855bb781579bf9e9742ed119fab4a20464
Marc Renken <marc.renken@bitbot.eu>
2023-01-15 13:54:34 +0100

    test(TemplateValues): Init

--- test/logger_dart_main.dart
+++ test/logger_dart_main.dart
@@ -1,5 +1,6 @@
 import 'logger_test.dart' as logger_test;
-import 'message_template_test.dart' as message_template_test;
+import 'message_template_values_test.dart' as message_template_values_test;
+import 'message_templates_test.dart' as message_templates_test;
 import 'messages_test.dart' as messages_test;
 import 'mixin/logging_test.dart' as logging_test;
 
@@ -7,5 +8,6 @@ void main() {
   logger_test.main();
   logging_test.main();
   messages_test.main();
-  message_template_test.main();
+  message_templates_test.main();
+  message_template_values_test.main();
 }
